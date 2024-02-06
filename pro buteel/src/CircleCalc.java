d796699eb268b869360bdcbcfb20445277c738a0
kiseeroo <teka0433@gmail.com>
2024-02-06 14:23:11 +0800

    first commit

---
+++ pro buteel/src/CircleCalc.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class CircleCalc {
+ private static final double PI = 3.14;
+ private final double radius;
+ public CircleCalc(double radius) {
+ this.radius = radius;
+ }
+ public static void main(String[] args) {
+ System.out.println("------------------------------------------------------------");
+ System.out.println("CircleCalc v1.0");
+ System.out.println();
+ System.out.println("Calculates and prints information for a user-supplied radius");
+ System.out.println("------------------------------------------------------------");
+ System.out.print("Enter the circle's radius: ");
+ Scanner in = new Scanner(System.in);
+ CircleCalc c = new CircleCalc(in.nextFloat());
+ System.out.print("Enter the circle's radius: ");
+ System.out.print("Enter the circle's radius: ");System.out.print("Enter the circle's radius: ");
+ System.out.print("Enter the circle's radius: ");
+ System.out.print("Enter the circle's radius: ");
+ System.out.print("Enter the circle's radius: ");
+ System.out.print("Enter the circle's radius: ");
+
+ System.out.print("Enter the circle's radius: ");
+
+ System.out.print("Enter the circle's radius: ");
+
+
+ 
+ // TODO: compute and print the circumference and area of the circle
+ }
+}
